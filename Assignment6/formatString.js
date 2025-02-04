d9045c2b5a1d909e44cfe812d6ff00bf74f6938b
jbirchfield1 <123132450+jbirchfield1@users.noreply.github.com>
2025-02-04 15:42:25 -0600

    Uploading Assignment 6 Attempt

---
+++ Assignment6/formatString.js
@@ -0,0 +1,13 @@
+function formatNames(names){
+    let output = "";
+    for(let i = 0; i < names.length;i++){
+            names[i] = names[i].charAt(0).toUpperCase() + names[i].slice(1);        //makes first letter uppercase
+            output += names[i]      //adds name to output
+        if(i<names.length-1)
+            output += ", "          //puts comma after name if it isn't the end
+    }
+    return output;
+}
+
+
+console.log(formatNames(["alice", "bob", "charlie"]));
