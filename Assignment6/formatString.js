7a9725bfa9af811a9c3bc86bde7a759b42c06dac
jbirchfield1 <123132450+jbirchfield1@users.noreply.github.com>
2025-02-07 19:01:12 -0600

    Adjusted comments on Assignment 6

--- Assignment6/formatString.js
+++ Assignment6/formatString.js
@@ -2,7 +2,7 @@ function formatNames(names){
     let output = "";
     for(let i = 0; i < names.length;i++){
             //names[i] = names[i].charAt(0).toUpperCase() + names[i].slice(1);        //makes first letter uppercase
-            output += names[i].charAt(0).toUpperCase() + names[i].slice(1);      //adds name to output
+            output += names[i].charAt(0).toUpperCase() + names[i].slice(1);      //adds name with uppercase first letter to output
         if(i<names.length-1)
             output += ", "          //puts comma after name if it isn't the end
     }
