f5b09d872e16216bb94d17ef15d5accafd49b058
jbirchfield1 <123132450+jbirchfield1@users.noreply.github.com>
2025-02-11 14:03:03 -0600

    Validation checks complete

--- JS Forms InClass/script.js
+++ JS Forms InClass/script.js
@@ -3,21 +3,35 @@ function validateForm(form){
     let name = document.forms["form1"]["name"].value;
     let email = document.forms["form1"]["email"].value;
     let age = document.forms["form1"]["age"].value;
+    let emailTest = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    let valid = true;
     if(name == "" ){
         document.getElementById("nameInput").style.backgroundColor = "red";
-        return false;
+        console.error("null name input");
+        valid = false;
     }
     if(email == ""){
         document.getElementById("emailInput").style.backgroundColor = "red";
-        return false;
+        console.error("null email input");
+        valid = false;
+    }
+    if(!emailTest.test(email)){
+        document.getElementById("emailInput").style.backgroundColor = "red";
+        console.error("invalid email input");
+        valid = false;
     }
     if(age<18 || age>100){
         document.getElementById("ageInput").style.backgroundColor = "red";
+        console.error("age outside of acceptable range");
+        valid = false;
     }
+    return valid;
 }
 function submitForm(form){
     let name = document.forms["form1"]["name"].value;
     let email = document.forms["form1"]["email"].value;
     let age = document.forms["form1"]["age"].value;
-    
+    if(validateForm(form)){
+        alert("Form Submitted Successfully!")
+    }
 }
