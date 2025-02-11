cb211cebf8eb6bfaf71754e70b4aa0490683e541
jbirchfield1 <123132450+jbirchfield1@users.noreply.github.com>
2025-02-11 15:06:28 -0600

    removed unnecessary variables in submitForm functions

--- JS Forms InClass/script.js
+++ JS Forms InClass/script.js
@@ -27,9 +27,6 @@ function validateForm(form){
     return valid;
 }
 function submitForm(form){
-    let name = document.forms["form1"]["name"].value;
-    let email = document.forms["form1"]["email"].value;
-    let age = document.forms["form1"]["age"].value;
     if(validateForm(form)){
         document.getElementById("results").innerHTML = "Form Submitted Sucessfully!";
         form.reset();
