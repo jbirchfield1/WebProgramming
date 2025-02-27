7771023b193e4d18c76ceada0732a7018be94a45
jbirchfield1 <123132450+jbirchfield1@users.noreply.github.com>
2025-02-26 18:58:34 -0600

    Made the buttons random colors

--- Assignment8/react-three-cards/src/Card.js
+++ Assignment8/react-three-cards/src/Card.js
@@ -8,12 +8,21 @@ function Card({title, content, color}){
         alert(title);
       };
 
-
+    const getRandomColor = () => {
+        let letters="0123456789ABCDEF";
+        let color="#";
+        for(let i=0;i<6;i++){
+            color+=letters[Math.floor(Math.random()*16)]
+        }
+        return color;
+        }
+    
+    let buttonColor = getRandomColor();
     return(
         <div className="card" style = {{backgroundColor:color}}>
             <h2>{title}</h2>
             <p>{content}</p>
-            <button onClick={handleClick}>Click Me!</button>
+            <button onClick={handleClick} style={{backgroundColor:buttonColor}}>Click Me!</button>
         </div>
     );
 }
