0b460cd3d3ad20145e4dad9178cda6b7d319c289
jbirchfield1 <123132450+jbirchfield1@users.noreply.github.com>
2025-02-28 13:30:36 -0600

    Finished Code Along 9

---
+++ Day12/use-state-practice/src/App.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+
+/* function countInitial(){
+  console.log("run function");
+  return 4;
+} */
+
+function App(){
+  const [count, setCount] = useState(4);
+  const [theme, setTheme] = useState('blue');
+
+
+  function decrementCount(){
+    setCount(prevCount => prevCount - 1);
+  }
+
+  function incrementCount(){
+    setCount(prevCount => prevCount + 1);
+    setTheme('red');
+  }
+
+  return(
+    <>
+    <button onClick={decrementCount}>-</button>
+    <span>{count}</span>
+    <span>{theme}</span>
+    <button onClick={incrementCount}>+</button>
+    </>
+  )
+}
+
+export default App;
