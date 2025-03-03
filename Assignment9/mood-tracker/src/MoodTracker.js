9e837c3ac9a7c3272c0a75aa4bb9cedc1dfc2280
jbirchfield1 <123132450+jbirchfield1@users.noreply.github.com>
2025-03-03 15:25:21 -0600

    Adding some details

--- Assignment9/mood-tracker/src/MoodTracker.js
+++ Assignment9/mood-tracker/src/MoodTracker.js
@@ -12,6 +12,24 @@ export default function MoodTracker(){
         setStory(prevStory => "I was happy once.")
     }
 
+    const Bored = () =>{
+        setMood(prevMood => "😒");
+        setSong(prevSong => "Mattel - Avenged Sevenfold");
+        setStory(prevStory => "I was bored once.")
+    }
+
+    const Upset = () =>{
+        setMood(prevMood => "😭");
+        setSong(prevSong => "Lonely Day - System of a Down");
+        setStory(prevStory => "I was upset once.")
+    }
+
+    const Angry = () =>{
+        setMood(prevMood => "😡");
+        setSong(prevSong => "I Hate Everything About You - Three Days Grace");
+        setStory(prevStory => "I was angry once.")
+    }
+
     return(
         <div style={{textAlign:"center", padding:"20px"}}>
             <h1>Mood Tracker</h1>
@@ -19,9 +37,9 @@ export default function MoodTracker(){
             <p>Related Song: {song}</p>
             <p>Story: {story}</p>
             <button onClick={VeryHappy} style={{margin: "5px", padding:"10px", background:"lightgreen"}}> Very Happy </button>
-            <button onClick={()=>setMood("😒")} style={{margin: "5px", padding:"10px", background:"lightblue"}}> Unamused </button>
-            <button onClick={()=>setMood("😭")} style={{margin: "5px", padding:"10px", background:"lightcoral"}}> Upset </button>
-            <button onClick={()=>setMood("😁")} style={{margin: "5px", padding:"10px", background:"lightgray"}}> Beaming </button>
+            <button onClick={Bored} style={{margin: "5px", padding:"10px", background:"lightblue"}}> Unamused </button>
+            <button onClick={Upset} style={{margin: "5px", padding:"10px", background:"lightcoral"}}> Upset </button>
+            <button onClick={Angry} style={{margin: "5px", padding:"10px", background:"lightgray"}}> Angry </button>
         </div>
     );
 }
