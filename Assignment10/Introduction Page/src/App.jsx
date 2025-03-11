85d1504d6487306c9e97f123816db4e7b4372bd6
jbirchfield1 <123132450+jbirchfield1@users.noreply.github.com>
2025-03-11 15:29:56 -0500

    styling updates

--- Assignment10/Introduction Page/src/App.jsx
+++ Assignment10/Introduction Page/src/App.jsx
@@ -7,19 +7,19 @@ function App() {
 
   return (
     <>
-    <div className='bg-blue-300 text-blue-950 rounded-xl'>
+    <div className='bg-blue-300 text-blue-950 rounded-xl font-mono'>
       <header>
-        <h1>Joshua Birchfield</h1>
+        <h1 className='text-4xl'>Joshua Birchfield</h1>
         <h2 className='text-xl'>Senior Computer Science Student</h2>
         <h3 className='text-lg'>Arkansas Tech University</h3>
       </header>
-      <main className='mt-3'>
+      <main className='mt-4'>
         <h3 className='text-lg'>Bio:</h3>
-        <p>My name is Joshua Birchfield and I come from Huntsville, Arkansas. I grew an interest in Computer Science through a class in High School and a love for videogames. I hope to make intuitive programs that make life a little more convenient.</p>
+        <p>My name is Joshua Birchfield and I come from Huntsville, Arkansas. I grew an interest in Computer Science through a class in High School and a love for videogames. I love things that cause me to learn and challenge myself. I hope to make intuitive programs that make life a little more convenient.</p>
 
-        <div className='p-3'>
+        <div className='p-2 mt-4'>
           <h2>Education:</h2>
-          <ul>
+          <ul className='ml-3'>
             <li>High School Diploma</li>
             <li>Certificate in Programming</li>
             <li>Associate's in I.T.</li>
@@ -30,10 +30,10 @@ function App() {
       <footer className='bg-blue-400'>
         <p>Contact Info:</p>
         <ul className='flex gap-3 ml-2'>
-          <li>Email: <a href="mailto:joshua.birchfield32@gmail.com">joshua.birchfield32@gmail.com</a></li>
+          <li>Email: <a href="mailto:joshua.birchfield32@gmail.com" className='text-purple-900'>joshua.birchfield32@gmail.com</a></li>
           <li>Phone Number: (479) 530 - 9156</li>
-          <li>X: <a href="https://x.com/JBirchfield32">Joshua Birchfield</a></li>
-          <li>LinkedIn: <a href="https://www.linkedin.com/in/joshua-birchfield-b13304295">Joshua Birchfield</a></li>
+          <li>X: <a href="https://x.com/JBirchfield32"className='text-purple-900'>Joshua Birchfield</a></li>
+          <li>LinkedIn: <a href="https://www.linkedin.com/in/joshua-birchfield-b13304295"className='text-purple-900'>Joshua Birchfield</a></li>
         </ul>
       </footer>
     </div>
