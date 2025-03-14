47a3d53a5cde739b4553a6b519c9d0392580440a
jbirchfield1 <123132450+jbirchfield1@users.noreply.github.com>
2025-03-14 09:18:35 -0500

    More styling changes

--- Assignment10/Introduction Page/src/App.jsx
+++ Assignment10/Introduction Page/src/App.jsx
@@ -7,19 +7,21 @@ function App() {
 
   return (
     <>
-    <div className='bg-blue-300 text-blue-950 rounded-xl font-mono  max-w-full max-h-full'>
-      <header className='border-b-4'>
-        <h1 className='text-4xl '>Joshua Birchfield</h1>
-        <h2 className='text-xl'>Senior Computer Science Student</h2>
-        <h3 className='text-lg'>Arkansas Tech University</h3>
-        <img  className='w-40' src='/profile.jpg'></img>
+    <div className='bg-blue-300 text-blue-950 rounded-xl font-mono  max-w-screen-md mx-auto'>
+      <header className='border-b-4 pb-4 text-center'>
+        <h1 className='text-4xl font-extrabold'>Joshua Birchfield</h1>
+        <h2 className='text-xl font-bold'>Senior Computer Science Student</h2>
+        <h3 className='text-lg font-semibold'>Arkansas Tech University</h3>
+        <img  className='w-40 sm:w-32 md:w-48 mx-auto rounded-xl mt-4' src='/profile.jpg'></img>
       </header>
       <main className='mt-4'>
-        <h3 className='text-lg'>Bio:</h3>
-        <p>My name is Joshua Birchfield and I come from Huntsville, Arkansas. I grew an interest in Computer Science through a class in High School and a love for videogames. I love things that cause me to learn and challenge myself. I hope to make intuitive programs that make life a little more convenient.</p>
+        <div className='p-2'>
+        <h3 className='text-lg font-bold'>Bio:</h3>
+        <p className='text-justify'>My name is Joshua Birchfield and I come from Huntsville, Arkansas. I grew an interest in Computer Science through a class in High School and a love for videogames. I love things that cause me to learn and challenge myself. I hope to make intuitive programs that make life a little more convenient.</p>
+        </div>
 
         <div className='p-2 mt-4'>
-          <h2>Education:</h2>
+          <h2 className='text-xl font-bold'>Education:</h2>
           <ul className='ml-3'>
             <li>High School Diploma</li>
             <li>Certificate in Programming</li>
@@ -28,9 +30,9 @@ function App() {
         </div>
       </main>
       
-      <footer className='bg-blue-400'>
-        <p>Contact Info:</p>
-        <ul className='flex flex-wrap gap-3 ml-2'>
+      <footer className='bg-blue-400 p-4 mt-6 text-center rounded-lg'>
+        <p className='font-bold'>Contact Info:</p>
+        <ul className='flex flex-wrap gap-3 ml-2 text-sm'>
           <li>Email: <a href="mailto:joshua.birchfield32@gmail.com" className='text-purple-900'>joshua.birchfield32@gmail.com</a></li>
           <li>Phone Number: (479) 530 - 9156</li>
           <li>X: <a href="https://x.com/JBirchfield32"className='text-purple-900'>Joshua Birchfield</a></li>
